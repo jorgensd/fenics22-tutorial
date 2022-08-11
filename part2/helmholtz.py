ed00590a578597c85f5930925edd60a4cddc6d7c
Adeeb Arif Kor <mabm4@cam.ac.uk>
2022-08-11 12:08:28 +0100

    Reorganise sections
    * Reorganise cells
    
    * Minor clean-up

--- part2/helmholtz.py
+++ part2/helmholtz.py
@@ -6,23 +6,24 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.14.1
+#       jupytext_version: 1.13.8
 #   kernelspec:
 #     display_name: Python 3 (DOLFINx complex)
 #     language: python
 #     name: python3-complex
 # ---
 
-# + [markdown] slideshow={"slide_type": "slide"} tags=[] jp-MarkdownHeadingCollapsed=true
+# + [markdown] slideshow={"slide_type": "slide"} tags=[] jp-MarkdownHeadingCollapsed=true tags=[] slideshow={"slide_type": "slide"}
 # # The Helmholtz equation
 #
 #
 # In this tutorial, we will learn:
-#  - How to discretize PDEs with complex-valued fields,
-#  - How to import and use high-order meshes from GMSH,
+#
+#  - How to solve PDEs with complex-valued fields,
+#  - How to import and use high-order meshes from Gmsh,
 #  - How to use high order discretizations,
 #  - How to use UFL expressions.
-# -
+# + [markdown] slideshow={"slide_type": "slide"} tags=[]
 # ## Problem statement
 # We will solve the Helmholtz equation subject to a first order absorbing boundary condition:
 #
@@ -31,57 +32,59 @@
 # \Delta u + k^2 u &= 0 \qquad \text{in } \Omega,\\
 # \nabla u \cdot \mathbf{n} - jku &= g \qquad \text{on } \partial\Omega.
 # \end{align*}
-# $$ 
+# $$
+#
 # where $k$ is a pieciwise constant wavenumber, and $g$ is the boundary source term computed as:
 #
 # $$g = \nabla u_i \cdot \mathbf{n} - jku_i$$
 #
 # and $u_i$ is the incoming plane wave. 
 
-# + slideshow={"slide_type": "slide"} tags=[]
+# + slideshow={"slide_type": "skip"} tags=[]
+import numpy as np
 from mpi4py import MPI
+
 from dolfinx import fem
 import ufl
 
-# + [markdown] slideshow={"slide_type": "notes"} tags=[]
-# We define some model parameters:
-
-# + slideshow={"slide_type": "fragment"} tags=[]
-import numpy as np
-
-# MPI communicator
-comm = MPI.COMM_WORLD
-
-# wavenumber in free space (air)
-k0 = 10 * np.pi
-# Corresponding wavelength
-lmbda = 2 * np.pi / k0
-
-# Polynomial degree
-degree = 4
-# Mesh order
-mesh_order = 2
-
-# + [markdown] slideshow={"slide_type": "skip"} tags=[]
+# + [markdown] slideshow={"slide_type": "slide"} tags=[]
 # This code is only meant to be executed with complex-valued degrees of freedom. To be able to solve such problems, we use the complex build of PETSc.
 
-# + slideshow={"slide_type": "skip"} tags=[]
+# + slideshow={"slide_type": "fragment"} tags=[]
 import sys
 from petsc4py import PETSc
+
 if not np.issubdtype(PETSc.ScalarType, np.complexfloating):
     print("This tutorial requires complex number support")
     sys.exit(0)
 else:
     print(f"Using {PETSc.ScalarType}.")
 
-# + [markdown] slideshow={"slide_type": "notes"} tags=[]
-# We will use [GMSH](http://gmsh.info/) to generate the computational domain (mesh) for this example. As long as GMSH has been installed (including its Python API), DOLFINx supports direct input of GMSH models (generated on one process). DOLFINx will then in turn distribute the mesh over all processes in the communicator passed to `dolfinx.io.gmshio.model_to_mesh`.
+# + [markdown] slideshow={"slide_type": "slide"} tags=[]
+# ## Defining model parameters
+
+# + slideshow={"slide_type": "fragment"} tags=[]
+# MPI communicator
+comm = MPI.COMM_WORLD
+
+# wavenumber in free space (air)
+k0 = 10 * np.pi
+
+# Corresponding wavelength
+lmbda = 2 * np.pi / k0
+
+# Polynomial degree
+degree = 4
+
+# Mesh order
+mesh_order = 2
 
 # + [markdown] slideshow={"slide_type": "slide"} tags=[]
 # ## Interfacing with GMSH
-
-# + [markdown] slideshow={"slide_type": "notes"} tags=[]
-# The function `generate_mesh` creates a GMSH model on rank 0 of `MPI.COMM_WORLD`.
+#
+# We will use [Gmsh](http://gmsh.info/) to generate the computational domain (mesh) for this example. As long as Gmsh has been installed (including its Python API), DOLFINx supports direct input of Gmsh models (generated on one process). DOLFINx will then in turn distribute the mesh over all processes in the communicator passed to `dolfinx.io.gmshio.model_to_mesh`.
+#
+# The function `generate_mesh` creates a Gmsh model on rank 0 of `MPI.COMM_WORLD`.The function `generate_mesh` creates a Gmsh model on rank 0 of `MPI.COMM_WORLD`.
 
 # + slideshow={"slide_type": "skip"} tags=[]
 try:
@@ -90,7 +93,7 @@ except ModuleNotFoundError:
     print("This demo requires gmsh to be installed")
     sys.exit(0)
 
-# + tags=[]
+# + tags=[] slideshow={"slide_type": "fragment"}
 import gmsh
 from dolfinx.io import gmshio
 from mesh_generation import generate_mesh
@@ -101,21 +104,21 @@ mesh, cell_tags, _ = gmshio.model_to_mesh(model, comm, 0, gdim=2)
 gmsh.finalize()
 
 # + [markdown] slideshow={"slide_type": "slide"} tags=[]
-# ## Material parameters and boundary conditions
-
-# + [markdown] slideshow={"slide_type": "notes"} tags=[]
+# ## Material parameters
+#
 # In this problem, the wave number in the different parts of the domain depends on cell markers, inputted through `cell_tags`.
 # We use the fact that a Discontinuous Galerkin space of order 0 (cell-wise piecewise constants) has a one-to-one mapping with the cells local to the process.
-# -
 
+# + slideshow={"slide_type": "fragment"} tags=[]
 DG = fem.FunctionSpace(mesh, ("DG", 0))
 k = fem.Function(DG)
 k.x.array[:] = k0
 k.x.array[cell_tags.find(1)] = 2*k0
 
+# + [markdown] slideshow={"slide_type": "subslide"} tags=[]
 # Now we can visualize the wavenumber distribution throughout the computational domain:
 
-# +
+# + slideshow={"slide_type": "fragment"} tags=[]
 import pyvista
 import matplotlib.pyplot as plt
 from dolfinx.plot import create_vtk_mesh
@@ -130,10 +133,12 @@ renderer = plotter.add_mesh(grid, show_edges=True)
 plotter.view_xy()
 plotter.show()
 
-# + [markdown] slideshow={"slide_type": "notes"} tags=[]
+# + [markdown] slideshow={"slide_type": "slide"} tags=[]
+# ## Boundary conditions
+#
 # Next, we define the boundary source term, by using `ufl.SpatialCoordinate`. When using this function, all quantities using this expression will be evaluated at quadrature points.
-# -
 
+# + slideshow={"slide_type": "fragment"} tags=[]
 n = ufl.FacetNormal(mesh)
 x = ufl.SpatialCoordinate(mesh)
 ui = ufl.exp(-1j * k * x[0])
@@ -141,21 +146,20 @@ g = ufl.dot(ufl.grad(ui), n) + 1j * k * ui
 
 # + [markdown] slideshow={"slide_type": "slide"} tags=[]
 # ## Variational form
-
-# + [markdown] slideshow={"slide_type": "notes"} tags=[]
+#
 # Next, we can define the variational problem, using a 4th order Lagrange space. Note that as we are using complex valued functions, we have to use the appropriate inner product, see [DOLFINx tutorial: Complex numbers](https://jorgensd.github.io/dolfinx-tutorial/chapter1/complex_mode.html) for more information.
 #
 #
 # $$ -\int_\Omega \nabla u \cdot \nabla \bar{v} ~ dx + \int_\Omega k^2 u \,\bar{v}~ dx - j\int_{\partial \Omega} ku  \bar{v} ~ ds = \int_{\partial \Omega} g \, \bar{v}~ ds \qquad \forall v \in \widehat{V}. $$
 
-# +
+# + slideshow={"slide_type": "fragment"} tags=[]
 element = ufl.FiniteElement("Lagrange", mesh.ufl_cell(), degree)
 V = fem.FunctionSpace(mesh, element)
 u = ufl.TrialFunction(V)
 v = ufl.TestFunction(V)
 
-
-a = -ufl.inner(ufl.grad(u), ufl.grad(v)) * ufl.dx \
+# + tags=[] slideshow={"slide_type": "fragment"}
+a = - ufl.inner(ufl.grad(u), ufl.grad(v)) * ufl.dx \
     + k**2 * ufl.inner(u, v) * ufl.dx \
     - 1j * k * ufl.inner(u, v) * ufl.ds
 L = ufl.inner(g, v) * ufl.ds
@@ -166,15 +170,17 @@ L = ufl.inner(g, v) * ufl.ds
 # + [markdown] slideshow={"slide_type": "notes"} tags=[]
 # Next, we will solve the problem using a direct solver (LU).
 
-# + tags=[]
+# + tags=[] slideshow={"slide_type": "fragment"}
 opt = {"ksp_type": "preonly", "pc_type": "lu"}
 problem = fem.petsc.LinearProblem(a, L, petsc_options=opt)
 uh = problem.solve()
 uh.name = "u"
 
 # + [markdown] slideshow={"slide_type": "slide"} tags=[]
-# ## Visualizing the complex solution
-# -
+# ## Postprocessing
+#
+# ### Visualising using PyVista
+# + slideshow={"slide_type": "fragment"} tags=[]
 topology, cells, geometry = create_vtk_mesh(V)
 grid = pyvista.UnstructuredGrid(topology, cells, geometry)
 grid.point_data["Re(u)"] = uh.x.array.real
@@ -198,16 +204,16 @@ def plot_function(grid, name):
 
 
 
-# + slideshow={"slide_type": "fragment"} tags=[]
+# + slideshow={"slide_type": "subslide"} tags=[]
 plot_function(grid, "Re(u)")
 
-# + slideshow={"slide_type": "notes"} tags=[]
+# + slideshow={"slide_type": "subslide"} tags=[]
 plot_function(grid, "Im(u)")
 
 # + [markdown] slideshow={"slide_type": "slide"} tags=[]
-# ## Saving higher order functions
+# ### Saving functions
 
-# + tags=[]
+# + tags=[] slideshow={"slide_type": "fragment"}
 from dolfinx.io import XDMFFile, VTXWriter
 
 # XDMF write the solution as a P1 function
