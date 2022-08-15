1cc27a121c3dd56255befd789a020291fabeba27
Jørgen Dokken <jsd55@cam.ac.uk>
2022-08-15 12:08:32 +0100

    Try to improve stokes. Hit various issues with: 1. Solvers not converging for non-nest approach 2. Custom elements not working on refined grids

--- part3/comparing_elements.py
+++ part3/comparing_elements.py
@@ -23,8 +23,7 @@
 # Import some stuff
 
 # + tags=[]
-from dolfinx import fem, mesh
-from dolfinx import cpp as _cpp
+from dolfinx import cpp, fem, mesh
 
 import ufl
 import numpy as np
@@ -43,33 +42,34 @@ import matplotlib.pylab as plt
 # TODO: some text here, probably split this code into multiple cells.
 
 # +
-def velocity(x, y):
-    a = 2*np.pi*ufl.sin(np.pi*x)*ufl.sin(np.pi*y)
-    b = ufl.sin(np.pi*x)*ufl.cos(np.pi*y)
-    c = ufl.cos(np.pi*x)*ufl.sin(np.pi*y)
+def u_ex(x):
+    sinx = ufl.sin(ufl.pi * x[0]) 
+    siny = ufl.sin(ufl.pi * x[1])
+    cosx = ufl.cos(ufl.pi * x[0])
+    cosy = ufl.cos(ufl.pi * x[1])
+    return 2*ufl.pi *sinx * siny * ufl.as_vector((cosy * sinx, - cosx * siny))
 
-    return (a*b, -a*c)
+def p_ex(x):
+    return ufl.sin(2*ufl.pi*x[0]) * ufl.sin(2*ufl.pi*x[1])
+
+def source(x):
+    u, p = u_ex(x), p_ex(x)
+    return - ufl.div(ufl.grad(u)) + ufl.grad(p)
 
 
-def rhs(x, y):
-    f1 = ufl.sin(2*np.pi*y)*(ufl.cos(2*np.pi*x) - 2*np.pi**2*ufl.cos(2*np.pi*x) + np.pi**2)
-    f2 = ufl.sin(2*np.pi*x)*(ufl.cos(2*np.pi*y) + 2*np.pi**2*ufl.cos(2*np.pi*y) - np.pi**2)
 
-    return(2*np.pi*f1, 2*np.pi*f2)
+def solve_stokes(u_element, p_element, domain):
+    V = fem.FunctionSpace(domain, u_element)
+    Q = fem.FunctionSpace(domain, p_element)
 
-
-def solve_stokes(element, domain):
-    V = fem.FunctionSpace(domain, element.sub_elements()[0])
-    Q = fem.FunctionSpace(domain, element.sub_elements()[1])
-    x = ufl.SpatialCoordinate(domain)
-    u_soln = ufl.as_vector(velocity(x[0], x[1]))
-
+    
     # Velocity boundary condition
     g = fem.Function(V)
     g.x.set(0.0)
-    bdry_facets = mesh.locate_entities_boundary(
-        domain, domain.topology.dim - 1, lambda x: np.ones(x.shape[1], dtype=np.bool8))
-    dofs = fem.locate_dofs_topological(V, domain.topology.dim-1, bdry_facets)
+    tdim = domain.topology.dim
+    domain.topology.create_connectivity(tdim - 1, tdim)
+    bdry_facets = mesh.exterior_facet_indices(domain.topology)
+    dofs = fem.locate_dofs_topological(V, tdim - 1, bdry_facets)
     bcv = fem.dirichletbc(g, dofs)
 
     bcs = [bcv]
@@ -77,15 +77,14 @@ def solve_stokes(element, domain):
     # Define variational problem
     u, p = ufl.TrialFunction(V), ufl.TrialFunction(Q)
     v, q = ufl.TestFunction(V), ufl.TestFunction(Q)
-    f = fem.Function(V)
-    f_expr = fem.Expression(ufl.as_vector(rhs(x[0], x[1])), V.element.interpolation_points())
-    f.interpolate(f_expr)
-    a = fem.form([[ufl.inner(ufl.grad(u), ufl.grad(v)) * ufl.dx, ufl.inner(p, ufl.div(v)) * ufl.dx],
-              [ufl.inner(ufl.div(u), q) * ufl.dx, None]])
-    L = fem.form([ufl.inner(f, v) * ufl.dx, ufl.inner(fem.Constant(domain, PETSc.ScalarType(0)), q) * ufl.dx])
+    x = ufl.SpatialCoordinate(domain)
+    f = source(x)
+    a_matrix = [[ufl.inner(ufl.grad(u), ufl.grad(v)) * ufl.dx, ufl.inner(p, ufl.div(v)) * ufl.dx],
+              [ufl.inner(ufl.div(u), q) * ufl.dx, None]]
+    a = fem.form(a_matrix)
+    L = fem.form([ufl.inner(f, v) * ufl.dx,  ufl.inner(fem.Constant(domain, PETSc.ScalarType(0)), q) * ufl.dx])
 
     # We will use a block-diagonal preconditioner to solve this problem:
-
     a_p11 = fem.form(ufl.inner(p, q) * ufl.dx)
     a_p = [[a[0][0], None],
            [None, a_p11]]
@@ -102,8 +101,8 @@ def solve_stokes(element, domain):
     P = PETSc.Mat().createNest([[A.getNestSubMatrix(0, 0), None], [None, P11]])
     P.assemble()
     b = fem.petsc.assemble_vector_nest(L)
-
     fem.petsc.apply_lifting_nest(b, a, bcs=bcs)
+
     # Sum contributions from ghost entries on the owner
     for b_sub in b.getNestSubVecs():
         b_sub.ghostUpdate(addv=PETSc.InsertMode.ADD, mode=PETSc.ScatterMode.REVERSE)
@@ -125,6 +124,7 @@ def solve_stokes(element, domain):
     nsp = PETSc.NullSpace().create(vectors=[null_vec])
     assert nsp.test(A)
     A.setNullSpace(nsp)
+  
     # Now we create a Krylov Subspace Solver `ksp`. We configure it to use
     # the MINRES method, and a block-diagonal preconditioner using PETSc's
     # additive fieldsplit type preconditioner:
@@ -150,28 +150,26 @@ def solve_stokes(element, domain):
     ksp_u.getPC().setType("gamg")
     ksp_p.setType("preonly")
     ksp_p.getPC().setType("jacobi")
-
-    # Monitor the convergence of the KSP
     ksp.setFromOptions()
+
     # Compute the solution
     u = fem.Function(V)
     p = fem.Function(Q)
-    x = PETSc.Vec().createNest([_cpp.la.petsc.create_vector_wrap(u.x), _cpp.la.petsc.create_vector_wrap(p.x)])
-    ksp.solve(b, x)
+    w = PETSc.Vec().createNest([u.vector, p.vector])
+    ksp.solve(b, w)
+    assert ksp.getConvergedReason() > 0
     u.x.scatter_forward()
     p.x.scatter_forward()
 
-    error = u - u_soln
-    velocity_error = domain.comm.allreduce(
-        fem.assemble.assemble_scalar(
-            fem.form(ufl.inner(error, error) * ufl.dx + ufl.inner(ufl.grad(error), ufl.grad(error)) * ufl.dx)) ** 0.5,
-            # fem.form(ufl.inner(error, error) * ufl.dx)) ** 0.5,
-        op=MPI.SUM)
-    
-    # TODO
-    from random import random
-    pressure_error = random()
-    
+    # Error computation
+    error_u = u - u_ex(x)
+    H1_u = fem.form(ufl.inner(error_u, error_u) * ufl.dx + ufl.inner(ufl.grad(error_u), ufl.grad(error_u)) * ufl.dx)
+    velocity_error = domain.comm.allreduce(np.sqrt(fem.assemble.assemble_scalar(H1_u)), op = MPI.SUM)
+    error_p = -p - p_ex(x)
+
+    L2_p = fem.form(error_p*error_p*ufl.dx)
+    pressure_error = domain.comm.allreduce(np.sqrt(fem.assemble.assemble_scalar(L2_p)), op = MPI.SUM)
+    print( ksp.getConvergedReason(), velocity_error, pressure_error)
     return velocity_error, pressure_error
 
 
@@ -179,21 +177,27 @@ def solve_stokes(element, domain):
 # TODO: some text here
 # -
 
-def compute_errors(element, nmeshes=5):
+def compute_errors(element_u, element_p, nmeshes=5):
     N0 = 10
-    hs = []
-    velocity_errors = []
-    pressure_errors = []
-    for k in range(nmeshes):
-        N = N0 * 2 ** k
-        hs.append(1./N)
+    hs = np.zeros(nmeshes)
+    u_errors = np.zeros(nmeshes)
+    p_errors = np.zeros(nmeshes)
+    for i in range(nmeshes):
+        # This works
+        # domain = mesh.create_rectangle(
+        #    MPI.COMM_WORLD, [np.array([0, 0]), np.array([1, 1])],
+        #    [N0*2**i, N0*2**i], mesh.CellType.triangle, mesh.GhostMode.none)
+        # This doesn't work
         domain = mesh.create_rectangle(
             MPI.COMM_WORLD, [np.array([0, 0]), np.array([1, 1])],
-            [N, N], mesh.CellType.triangle, mesh.GhostMode.none)
-        e1, e2 = solve_stokes(element, domain)
-        velocity_errors.append(e1)
-        pressure_errors.append(e2)
-    return hs, velocity_errors, pressure_errors
+            [N0, N0], mesh.CellType.triangle, mesh.GhostMode.none)
+        for j in range(i):
+            domain.topology.create_entities(1)
+            domain = mesh.refine(domain)
+        u_errors[i] , p_errors[i] = solve_stokes(element_u, element_p, domain)
+        num_local_cells = domain.topology.index_map(domain.topology.dim).size_local
+        hs[i] = np.max(cpp.mesh.h(domain, domain.topology.dim, np.arange(num_local_cells, dtype=np.int32)))
+    return hs, u_errors, p_errors
 
 
 # + [markdown] slideshow={"slide_type": "subslide"} tags=[]
@@ -204,16 +208,17 @@ def compute_errors(element, nmeshes=5):
 # +
 # %matplotlib inline
 
-def error_plot(element, convergence=None, nmeshes=5):
-    hs, v_errors, p_errors = compute_errors(element, nmeshes)
+def error_plot(element_u, element_p, convergence=None, nmeshes=5):
+    hs, v_errors, p_errors = compute_errors(element_u, element_p, nmeshes)
 
     legend = []
     if convergence is not None:
         y_value = 1.2 * v_errors[0]
         plt.plot([hs[0], hs[-1]], [y_value, y_value * (hs[-1] / hs[0])**convergence], "k--")
         legend.append(f"order {convergence}")
+    print(hs, v_errors, p_errors)
     plt.plot(hs, v_errors, "bo-")
-    # plt.plot(hs, p_errors, "ro-")
+    plt.plot(hs, p_errors, "ro-")
     legend += ["velocity", "pressure"]
     plt.legend(legend)
     plt.xscale("log")
@@ -228,33 +233,22 @@ def error_plot(element, convergence=None, nmeshes=5):
 
 # For our first element, we pair piecewise linear elements with piecewise constants. Using these elements, we do not converge to the solution.
 
-# +
-element = ufl.MixedElement(
-    ufl.VectorElement("Lagrange", "triangle", 1),
-    ufl.FiniteElement("DG", "triangle", 0))
-
-error_plot(element)
-# -
+element_u = ufl.VectorElement("Lagrange", "triangle", 1)
+element_p = ufl.FiniteElement("DG", "triangle", 0)
+error_plot(element_u, element_p)
 
 # One way to obtain convergence with a piecewise constant pressure space is to use a piecewise quadratic space for the velocity (Fortin, 1972).
 
-# +
-element = ufl.MixedElement(
-    ufl.VectorElement("Lagrange", "triangle", 2),
-    ufl.FiniteElement("DG", "triangle", 0))
-
-error_plot(element, 1)
-# -
+element_u = ufl.VectorElement("Lagrange", "triangle", 2)
+element_p = ufl.FiniteElement("DG", "triangle", 0)
+error_plot(element_u, element_p, 1)
 
 
 # Alternatively, the same order convergence can be achieved using fewer degrees of freedom if a Crouzeix-Raviart element is used for the velocity space (Crouziex, Raviart, 1973).
 
-# +
-element = ufl.MixedElement(
-    ufl.VectorElement("CR", "triangle", 1),
-    ufl.FiniteElement("DG", "triangle", 0))
-
-error_plot(element, 1)
+element_u = ufl.VectorElement("CR", "triangle", 1)
+element_p = ufl.FiniteElement("DG", "triangle", 0)
+error_plot(element_u, element_p, 1)
 
 # + [markdown] tags=[]
 # ## Piecewise linear pressure space
@@ -262,15 +256,9 @@ error_plot(element, 1)
 
 # When using a piecewise linear pressure space, we could again try using a velocity space one degree higher, but we would again observe that there is no convergence. In order to achieve convergence, we can augment the quadratic space with a cubic bubble function on the triangle (Crouziex, Falk, 1988).
 
-# +
-element = ufl.MixedElement(
-    ufl.VectorElement(ufl.EnrichedElement(
-        ufl.FiniteElement("Lagrange", "triangle", 2),
-        ufl.FiniteElement("Bubble", "triangle", 3))),
-    ufl.FiniteElement("DG", "triangle", 1))
-
-error_plot(element, 2)
-# -
+element_u = ufl.VectorElement(ufl.FiniteElement("Lagrange", "triangle", 2) + ufl.FiniteElement("Bubble", "triangle", 3))
+element_p = ufl.FiniteElement("DG", "triangle", 1)
+error_plot(element_u, element_p, 2)
 
 # ## Piecewise quadratic pressure space
 
@@ -307,13 +295,9 @@ M[2].append(np.zeros((0, 1, 0, 1)))
 
 p3_without_bubble = basix.ufl_wrapper.BasixElement(basix.create_custom_element(
     basix.CellType.triangle, [], wcoeffs, x, M, 0, basix.MapType.identity, False, 2, 3))
-element = ufl.MixedElement(
-    ufl.VectorElement(ufl.EnrichedElement(
-        p3_without_bubble,
-        ufl.FiniteElement("Bubble", "triangle", 4))),
-    ufl.FiniteElement("DG", "triangle", 2))
-
-error_plot(element, 3, 3)
+element_u = ufl.VectorElement(ufl.EnrichedElement(p3_without_bubble,ufl.FiniteElement("Bubble", "triangle", 4)))
+element_p = ufl.FiniteElement("DG", "triangle", 2)
+error_plot(element_u, element_p, 3, 3)
 # -
 
 # This last example is converging with the wrong order... (Crouzeix, Falk, 1988)
@@ -338,11 +322,9 @@ M[2].append(np.array([[[[1.]]]]))
 
 crouzeix_falk = basix.ufl_wrapper.BasixElement(basix.create_custom_element(
     basix.CellType.triangle, [], wcoeffs, x, M, 0, basix.MapType.identity, False, 3, 3))
-element = ufl.MixedElement(
-    ufl.VectorElement(crouzeix_falk),
-    ufl.FiniteElement("DG", "triangle", 2))
-
-error_plot(element, 3, 3)
+element_u = ufl.VectorElement(crouzeix_falk)
+element_p = ufl.FiniteElement("DG", "triangle", 2)
+error_plot(element_u, element_p, 3, 3)
 # -
 
 # ## References
@@ -352,3 +334,7 @@ error_plot(element, 3, 3)
 # Crouzeix, Michel and Raviart, Pierre-Arnaud. Conforming and nonconforming finite element methods for solving the stationary Stokes equations, *Revue Française d'Automatique, Informatique et Recherche Opérationnelle* 3, 33–75, 1973. [DOI: [10.1051/m2an/197307R300331](https://doi.org/10.1051/m2an/197307R300331)]
 #
 # Fortin, Michel. Calcul numérique des écoulements des fluides de Bingham et des fluides newtoniens incompressibles par la méthode des éléments finis (PhD thesis), Univ. Paris, 1972. 
+
+
+
+
