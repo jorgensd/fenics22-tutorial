b148eba6bab53dd9f006279f85464bb14a56f7ea
Jørgen Dokken <jsd55@cam.ac.uk>
2022-08-15 17:39:11 +0100

    Refactor Stokes code

--- part3/comparing_elements.py
+++ part3/comparing_elements.py
@@ -146,7 +146,7 @@ def create_velocity_bc(V):
 # This means that the problem is singular, i.e. the pressure is only determined up to a constant. We therefore create a PETSc nullspace operator for the pressure.
 
 # + slideshow={"slide_type": "fragment"} tags=[]
-def create_nullspace(lhs_form, rhs_form):
+def create_nullspace(rhs_form):
     null_vec = fem.petsc.create_vector_nest(rhs_form)
     null_vecs = null_vec.getNestSubVecs()
     null_vecs[0].set(0.0)
@@ -165,26 +165,97 @@ def create_nullspace(lhs_form, rhs_form):
 # The bottom-right diagonal entry is a mass matrix.
 # -
 
-def create_preconditioner(Q, a_uu):
+def create_preconditioner(Q, a, bcs):
     p, q = TrialFunction(Q), TestFunction(Q)
     a_p11 = fem.form(inner(p, q) * dx)
-    a_p = fem.form([[a_uu, None],
+    a_p = fem.form([[a[0][0], None],
                     [None, a_p11]])
-    P = fem.petsc.assemble_matrix_nest(a_p)
+    P = fem.petsc.assemble_matrix_nest(a_p, bcs)
     P.assemble()
     return P
 
 
+# + [markdown] slideshow={"slide_type": "slide"} tags=[]
+# ## Assemble nest system
+# -
+
+def assemble_system(lhs_form, rhs_form, bcs):
+    A = fem.petsc.assemble_matrix_nest(lhs_form, bcs=bcs)
+    A.assemble()
+    
+    b = fem.petsc.assemble_vector_nest(rhs_form)
+    fem.petsc.apply_lifting_nest(b, lhs_form, bcs=bcs)
+    for b_sub in b.getNestSubVecs():
+        b_sub.ghostUpdate(addv=PETSc.InsertMode.ADD,
+                          mode=PETSc.ScatterMode.REVERSE)
+    spaces = fem.extract_function_spaces(rhs_form)
+    bcs0 = fem.bcs_by_block(spaces, bcs)
+    fem.petsc.set_bc_nest(b, bcs0)
+    return A, b
+
+
+# + [markdown] slideshow={"slide_type": "slide"} tags=[]
+# ## PETSc Krylov Subspace solver
+
+# + [markdown] slideshow={"slide_type": "notes"} tags=[]
+# In legacy DOLFIN, convenience functions were provided to interact with linear algebra packages such as PETSc.
+# In DOLFINx, we instead rely on supplying the users with the appropriate data types for the backend. Then the user can access all of the features of the backend, not being constrained to the layer added in DOLFIN. One can also leverage the detailed documentation of PETSc. For block systems, see: https://petsc.org/release/docs/manual/ksp/?highlight=matnest#solving-block-matrices
+# -
+
+def create_block_solver(A, b, P, comm):
+    ksp = PETSc.KSP().create(comm)
+    ksp.setOperators(A, P)
+    ksp.setType("minres")
+    ksp.setTolerances(rtol=1e-9)
+    ksp.getPC().setType("fieldsplit")
+    ksp.getPC().setFieldSplitType(PETSc.PC.CompositeType.ADDITIVE)
+
+    nested_IS = P.getNestISs()
+    ksp.getPC().setFieldSplitIS(("u", nested_IS[0][0]),
+                                ("p", nested_IS[0][1]))
+
+    # Set the preconditioners for each block
+    ksp_u, ksp_p = ksp.getPC().getFieldSplitSubKSP()
+    ksp_u.setType("preonly")
+    ksp_u.getPC().setType("gamg")
+    ksp_p.setType("preonly")
+    ksp_p.getPC().setType("jacobi")
+
+    # Monitor the convergence of the KSP
+    ksp.setFromOptions()
+    return ksp
+
+
+# + [markdown] slideshow={"slide_type": "slide"} tags=[]
+# ## Compute error estimates
+
+# + [markdown] slideshow={"slide_type": "notes"} tags=[]
+# In DOLFINx, assembling a scalar value does require any MPI-communication. `dolfinx.fem.petsc.assemble_scalar` will only integrate over the cells owned by the process.
+# It is up to the user to gather the results, which can be gathered on for instance all processes, or a single process for outputting.
 
 # +
-
-
-
 def assemble_scalar(J, comm: MPI.Comm):
     scalar_form = fem.form(J)
     local_J = fem.assemble_scalar(scalar_form)
     return comm.allreduce(local_J, op=MPI.SUM)
 
+def compute_errors(u, p):
+    domain = u.function_space.mesh
+    x = SpatialCoordinate(domain)
+    error_u = u - u_ex(x)
+    H1_u = inner(error_u, error_u) * dx \
+         + inner(grad(error_u), grad(error_u)) * dx
+    velocity_error = np.sqrt(assemble_scalar(H1_u, domain.comm))
+
+    error_p = -p - p_ex(x)
+    L2_p = fem.form(error_p * error_p * dx)
+    pressure_error = np.sqrt(assemble_scalar(L2_p, domain.comm))
+    return velocity_error, pressure_error
+
+
+# + [markdown] slideshow={"slide_type": "slide"} tags=[]
+# ## Solving the Stokes problem with a block-preconditioner
+# -
 
 def solve_stokes(u_element, p_element, domain):
     V = fem.FunctionSpace(domain, u_element)
@@ -194,77 +265,28 @@ def solve_stokes(u_element, p_element, domain):
     rhs_form = create_linear_form(V, Q)
     
     bcs = create_velocity_bc(V)
-    nsp = create_nullspace(lhs_form, rhs_form)
-    
-    A = fem.petsc.assemble_matrix_nest(lhs_form, bcs=bcs)
-    A.assemble()
+    nsp = create_nullspace(rhs_form)
+    A, b = assemble_system(lhs_form, rhs_form, bcs)
     assert nsp.test(A)
     A.setNullSpace(nsp)
-    
-    P = create_preconditioner(Q, lhs_form[0][0])
-    
-    # Assemble rhs and create boundary conditions
-    b = fem.petsc.assemble_vector_nest(rhs_form)
-    fem.petsc.apply_lifting_nest(b, lhs_form, bcs=bcs)
-    for b_sub in b.getNestSubVecs():
-        b_sub.ghostUpdate(addv=PETSc.InsertMode.ADD, mode=PETSc.ScatterMode.REVERSE)
-    bcs0 = fem.bcs_by_block(fem.extract_function_spaces(rhs_form), bcs)
-    fem.petsc.set_bc_nest(b, bcs0)
-    
-    from IPython import embed;embed()
-    # Now we create a Krylov Subspace Solver `ksp`. We configure it to use
-    # the MINRES method, and a block-diagonal preconditioner using PETSc's
-    # additive fieldsplit type preconditioner:
 
-    # +
-    ksp = PETSc.KSP().create(domain.comm)
-    ksp.setOperators(A, P)
-    ksp.setType("minres")
-    ksp.setTolerances(rtol=1e-9)
-    ksp.getPC().setType("fieldsplit")
-    ksp.getPC().setFieldSplitType(PETSc.PC.CompositeType.ADDITIVE)
-
-    # Define the matrix blocks in the preconditioner with the velocity and
-    # pressure matrix index sets
-    nested_IS = P.getNestISs()
-    ksp.getPC().setFieldSplitIS(
-        ("u", nested_IS[0][0]),
-        ("p", nested_IS[0][1]))
-
-    # Set the preconditioners for each block
-    ksp_u, ksp_p = ksp.getPC().getFieldSplitSubKSP()
-    ksp_u.setType("preonly")
-    ksp_u.getPC().setType("gamg")
-    ksp_p.setType("preonly")
-    ksp_p.getPC().setType("jacobi")
-    ksp.setFromOptions()
-
-    # Compute the solution
-    u = fem.Function(V)
-    p = fem.Function(Q)
+    P = create_preconditioner(Q, lhs_form, bcs)
+    ksp = create_block_solver(A, b, P, domain.comm)
+ 
+    u, p = fem.Function(V), fem.Function(Q)
     w = PETSc.Vec().createNest([u.vector, p.vector])
     ksp.solve(b, w)
-    print( ksp.getConvergedReason())
     assert ksp.getConvergedReason() > 0
     u.x.scatter_forward()
     p.x.scatter_forward()
-
-    # Error computation
-    error_u = u - u_ex(x)
-    H1_u = inner(error_u, error_u) * dx + inner(grad(error_u), grad(error_u)) * dx
-    velocity_error = np.sqrt(assemble_scalar(H1_u, domain.comm))
-    error_p = -p - p_ex(x)
-    L2_p = fem.form(error_p * error_p * dx)
-    pressure_error = np.sqrt(assemble_scalar(L2_p, domain.comm))
-    return velocity_error, pressure_error
+    return compute_errors(u, p)
 
 
-# + [markdown] slideshow={"slide_type": "notes"} tags=[]
-# TODO: some text here
-# -
-
-def compute_errors(element_u, element_p, refinements=5):
-    N0 = 7
+# + [markdown] tags=[] slideshow={"slide_type": "notes"}
+# We now use the Stokes solver we have defined to experiment with a range of element pairs that can be used. First, we define a function that takes an element as input and plots a graph showing the error as $h$ is decreased.
+# + tags=[] slideshow={"slide_type": "skip"}
+def error_plot(element_u, element_p, convergence_u=None,
+               convergence_p=None, refinements=5, N0=7):
     hs = np.zeros(refinements)
     u_errors = np.zeros(refinements)
     p_errors = np.zeros(refinements)
@@ -275,27 +297,20 @@ def compute_errors(element_u, element_p, refinements=5):
                                          cell_type=mesh.CellType.triangle)
         u_errors[i] , p_errors[i] = solve_stokes(element_u, element_p, domain)
         hs[i] = 1. / N
-    return hs, u_errors, p_errors
-
-
-# + [markdown] slideshow={"slide_type": "subslide"} tags=[]
-# ## The elements
-
-# + [markdown] tags=[]
-# We now use the Stokes solver we have defined to experiment with a range of element pairs that can be used. First, we define a function that takes an element as input and plots a graph showing the error as $h$ is decreased.
-# -
-def error_plot(element_u, element_p, convergence=None, nmeshes=5):
-    hs, v_errors, p_errors = compute_errors(element_u, element_p, nmeshes)
-
     legend = []
-    if convergence is not None:
-        y_value = v_errors[-1] * 1.4
-        plt.plot([hs[0], hs[-1]], [y_value * (hs[0] / hs[-1])**convergence, y_value], "k--")
-        legend.append(f"order {convergence}")
-    print(hs, v_errors, p_errors)
-    plt.plot(hs, v_errors, "bo-")
+    
+    if convergence_u is not None:
+        y_value = u_errors[-1] * 1.4
+        plt.plot([hs[0], hs[-1]], [y_value * (hs[0] / hs[-1])**convergence_u, y_value], "k--")
+        legend.append(f"order {convergence_u}")
+    if convergence_p is not None:
+        y_value = p_errors[-1] * 1.4
+        plt.plot([hs[0], hs[-1]], [y_value * (hs[0] / hs[-1])**convergence_p, y_value], "m--")
+        legend.append(f"order {convergence_p}")
+
+    plt.plot(hs, u_errors, "bo-")
     plt.plot(hs, p_errors, "ro-")
-    legend += ["velocity", "pressure"]
+    legend += [r"$H^1(\mathbf{u_h}-\mathbf{u}_{ex})$", r"$L^2(p_h-p_ex)$"]
     plt.legend(legend)
     plt.xscale("log")
     plt.yscale("log")
@@ -304,59 +319,81 @@ def error_plot(element_u, element_p, convergence=None, nmeshes=5):
     plt.xlabel("$h$")
     plt.xlim(plt.xlim()[::-1])
     plt.grid(True)
-# + [markdown] tags=[]
+# + [markdown] tags=[] slideshow={"slide_type": "slide"}
 # ## Piecewise constant pressure spaces
+
+# + [markdown] slideshow={"slide_type": "notes"} tags=[]
+# For our first element, we pair piecewise linear elements with piecewise constants.
 # -
 
-# For our first element, we pair piecewise linear elements with piecewise constants.
-#
 # <img src='./img/element-Lagrange-triangle-1-dofs-large.png' style='width:100px' /><img src='./img/element-Lagrange-triangle-0-dofs-large.png' style='width:100px' />
-#
+
+# + [markdown] slideshow={"slide_type": "skip"} tags=[]
 # Using these elements, we do not converge to the solution.
+# -
 
 element_u = VectorElement("Lagrange", "triangle", 1)
 element_p = FiniteElement("DG", "triangle", 0)
 error_plot(element_u, element_p)
 
+# + [markdown] slideshow={"slide_type": "slide"} tags=[]
+# ## P2-DG0 (Fortin, 1972)
+
+# + [markdown] slideshow={"slide_type": "notes"} tags=[]
 # One way to obtain convergence with a piecewise constant pressure space is to use a piecewise quadratic space for the velocity (Fortin, 1972).
-#
+# -
+
 # <img src='./img/element-Lagrange-triangle-2-dofs-large.png' style='width:100px' /><img src='./img/element-Lagrange-triangle-0-dofs-large.png' style='width:100px' />
 
 element_u = VectorElement("Lagrange", "triangle", 2)
 element_p = FiniteElement("DG", "triangle", 0)
-error_plot(element_u, element_p, 1)
+error_plot(element_u, element_p, 2, 1)
 
 
+# + [markdown] slideshow={"slide_type": "slide"} tags=[]
+# ## Crouzeix-Raviart
+
+# + [markdown] slideshow={"slide_type": "notes"} tags=[]
 # Alternatively, the same order convergence can be achieved using fewer degrees of freedom if a Crouzeix-Raviart element is used for the velocity space (Crouziex, Raviart, 1973).
-#
+# -
+
 # <img src='./img/element-Crouzeix-Raviart-triangle-1-dofs-large.png' style='width:100px' /><img src='./img/element-Lagrange-triangle-0-dofs-large.png' style='width:100px' />
 
 element_u = VectorElement("CR", "triangle", 1)
 element_p = FiniteElement("DG", "triangle", 0)
 error_plot(element_u, element_p, 1)
 
-# + [markdown] tags=[]
-# ## Piecewise linear pressure space
+# + [markdown] tags=[] slideshow={"slide_type": "slide"}
+# ## Piecewise linear pressure space (Crouziex, Falk, 1988)
+
+# + [markdown] slideshow={"slide_type": "notes"} tags=[]
+# When using a piecewise linear pressure space, we could again try using a velocity space one degree higher, but we would again observe that there is no convergence. In order to achieve convergence, we can augment the quadratic space with a cubic bubble function on the triangle (Crouziex, Falk, 1988).
 # -
 
-# When using a piecewise linear pressure space, we could again try using a velocity space one degree higher, but we would again observe that there is no convergence. In order to achieve convergence, we can augment the quadratic space with a cubic bubble function on the triangle (Crouziex, Falk, 1988).
-#
 # <img src='./img/element-bubble-enriched-Lagrange-triangle-2-dofs-large.png' style='width:100px' /><img src='./img/element-Lagrange-triangle-1-dofs-large.png' style='width:100px' />
 
-enriched_element = FiniteElement("Lagrange", "triangle", 2) + FiniteElement("Bubble", "triangle", 3)
+enriched_element = FiniteElement("Lagrange", "triangle", 2) \
+                 + FiniteElement("Bubble", "triangle", 3)
 element_u = VectorElement(enriched_element)
 element_p = FiniteElement("DG", "triangle", 1)
 error_plot(element_u, element_p, 2)
 
-# ## Piecewise quadratic pressure space
+# + [markdown] slideshow={"slide_type": "slide"} tags=[]
+# ## Piecewise quadratic pressure space (Crouzeix, Falk, 1988)
 
+# + [markdown] slideshow={"slide_type": "notes"} tags=[]
 # When using a piecewise quadratic space, we can use a cubic velocity space augmented with quartic bubbles (Crouzeix, Falk, 1988).
-#
+# -
+
 # <img src='./img/element-bubble-enriched-Lagrange-triangle-3-dofs-large.png' style='width:100px' /><img src='./img/element-Lagrange-triangle-2-dofs-large.png' style='width:100px' />
-#
+
+# ### Custom elements
+
+# + [markdown] slideshow={"slide_type": "notes"} tags=[]
 # We have to define this velocity element as a custom element (it cannot be created as an enriched element, as the basis functions of degree 3 Lagrange and degree 4 bubbles are not linearly independent). More examples of how custom elements can be created can be found [in the Basix documentation](https://docs.fenicsproject.org/basix/v0.5.0/python/demo/demo_custom_element.py.html).
 
-# ### Defining the polynomial space
+# + [markdown] slideshow={"slide_type": "notes"} tags=[]
+# #### Defining the polynomial space
 #
 # When creating a custom element, we must input the coefficients that define a basis of the set of polynomials that our element spans. In this example, we will represent the 9 functions in our space in terms of the 10 orthonormal polynomials of degree $\leqslant3$ on a quadrilateral, so we create a 9 by 10 matrix.
 #
@@ -364,6 +401,7 @@ error_plot(element_u, element_p, 2)
 # $$f(x,y)=\sum_i\left(\int_0^1\int_0^{1-y}f(x, y)q_i(x, y) \,\mathrm{d}x\,\mathrm{d}y\right)q_i(x,y),$$
 # where $q_0$, $q_1$, ... are the orthonormal polynomials. The entries of our coefficient matrix are these integrals.
 
+# + slideshow={"slide_type": "notes"} tags=[]
 wcoeffs = np.zeros((12, 15))
 pts, wts = basix.make_quadrature(basix.CellType.triangle, 8)
 poly = basix.tabulate_polynomials(basix.PolynomialType.legendre, basix.CellType.triangle, 4, pts)
@@ -376,11 +414,12 @@ for j, f in enumerate([
     for i in range(15):
         wcoeffs[j, i] = sum(f * poly[i, :] * wts)
 
-# ### Interpolation
+# + [markdown] slideshow={"slide_type": "notes"} tags=[]
+# #### Interpolation
 #
 # Next, we compute the points and matrices that define how functions can be interpolated into this space. For this element, all the DOFs are point evaluations, so we create lists of these points and (reshaped) identity matrices.
 
-# +
+# + slideshow={"slide_type": "notes"} tags=[]
 x = [[], [], [], []]
 x[0].append(np.array([[0.0, 0.0]]))
 x[0].append(np.array([[1.0, 0.0]]))
@@ -396,18 +435,19 @@ for _ in range(3):
 for _ in range(3):
     M[1].append(np.array([[[[1.], [0.]]], [[[0.], [1.]]]]))
 M[2].append(np.array([[[[1.], [0.], [0.]]], [[[0.], [1.], [0.]]], [[[0.], [0.], [1.]]]]))
-# -
 
-# ### Creating the element
+# + [markdown] slideshow={"slide_type": "notes"} tags=[]
+# #### Creating the element
 #
 # We now create the element by passing in the information we created above, as well as the cell type, value shape, number of derivatives used by the DOFs, map type, whether the element is discontinuous, the highest degree Lagrange space that is a subspace of the element, and the polynomial degree of the element.
+# -
 
-p3_plus_bubbles = basix.ufl_wrapper.BasixElement(basix.create_custom_element(
-    basix.CellType.triangle, [], wcoeffs, x, M, 0, basix.MapType.identity, False, 3, 4))
-element_u = VectorElement(p3_plus_bubbles)
+p3_plus_bubbles = basix.create_custom_element(basix.CellType.triangle, [], wcoeffs, x, M, 0, basix.MapType.identity, False, 3, 4)
+element_u = VectorElement(basix.ufl_wrapper.BasixElement(p3_plus_bubbles))
 element_p = FiniteElement("DG", "triangle", 2)
-error_plot(element_u, element_p, 3, 4)
+error_plot(element_u, element_p, 3, refinements=4)
 
+# + [markdown] slideshow={"slide_type": "slide"} tags=[]
 # ## References
 #
 # Crouzeix, Michel and Falk, Richard S. Nonconforming finite elements for the Stokes problem, *Mathematics of Computation* 52, 437–456, 1989. [DOI: [10.2307/2008475](https://doi.org/10.2307/2008475)]
@@ -418,5 +458,6 @@ error_plot(element_u, element_p, 3, 4)
 #
 # The images of elements used in this example were taken from DefElement:<br />
 # The DefElement contributors. DefElement: an encyclopedia of finite element definitions, 2022, https://defelement.com [Online; accessed: 15-August-2022].
+# -
 
 
