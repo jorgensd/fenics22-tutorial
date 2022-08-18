5c0ce871e4649a049e434bf6fa1fcfa69ceacc77
Jørgen Dokken <dokken92@gmail.com>
2022-08-18 09:29:34 +0100

    More restructuring

--- jupyter_server_config.py
+++ jupyter_server_config.py
@@ -1,16 +1,17 @@
 import os
 from subprocess import check_call
 
+
 def post_save(model, os_path, contents_manager):
     """post-save hook for converting notebooks to .py scripts"""
     if model['type'] != 'notebook':
-        return # only do this for notebooks
+        return  # only do this for notebooks
     d, fname = os.path.split(os_path)
     if ".py" in fname:
-        return # Ignore python files
+        return  # Ignore python files
     check_call(['jupyter', 'nbconvert', '--to', "html",
                 "--TagRemovePreprocessor.remove_input_tags={\'remove-input\', \'hide-input\'}",
-                fname, "--template", "reveal", f"--output=html/{fname.split('.ipynb')[0]}"], cwd=d)
+                fname, "--template", "reveal", f"--output=presentation-{fname.split('.ipynb')[0]}"], cwd=d)
+
 
 c.FileContentsManager.post_save_hook = post_save
-
