7c55c138d2c389435014b3a1f6b5ff00f6c01ae7
jorgensd <dokken92@gmail.com>
2024-10-27 20:55:51 +0100

    Use ruff for formatting

--- jupyter_server_config.py
+++ jupyter_server_config.py
@@ -1,19 +1,33 @@
 import os
 from subprocess import check_call
 
+from traitlets.config import Config
+
 
 def post_save(model, os_path, contents_manager):
     """post-save hook for converting notebooks to .py scripts"""
-    print(model['type'])
-    if model['type'] != 'notebook':
+    print(model["type"])
+    if model["type"] != "notebook":
         return  # only do this for notebooks
     d, fname = os.path.split(os_path)
     if ".py" in fname:
         return  # Ignore python files
 
-    check_call(['jupyter', 'nbconvert', '--to', "html",
-                "--TagRemovePreprocessor.remove_input_tags={\'remove-input\', \'hide-input\'}",
-                fname, "--template", "reveal", f"--output=presentation-{fname.split('.ipynb')[0]}"], cwd=d)
+    check_call(
+        [
+            "jupyter",
+            "nbconvert",
+            "--to",
+            "html",
+            "--TagRemovePreprocessor.remove_input_tags={'remove-input', 'hide-input'}",
+            fname,
+            "--template",
+            "reveal",
+            f"--output=presentation-{fname.split('.ipynb')[0]}",
+        ],
+        cwd=d,
+    )
 
 
+c = Config()
 c.FileContentsManager.post_save_hook = post_save
